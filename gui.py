645281495e1cb77a9c8089e5261175d5f2a092d2
Artur <arturpietrzak522@o2.pl>
2020-05-03 18:38:47 +0200

    Update gui.py
    Dodano generację dróg prostych ze wszystkimi potrzebnymi narzędziami i zmienną przechowującą aktualny wybór klocka oraz zmieniono lekko pozycję canvas. Naprawiono errory żeby pokazywały odpowiednie komunikaty.

--- gui.py
+++ gui.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as msg
-import numpy as np
-from resizer import fit_tk, array_to_tk
+from math import floor, ceil
+from tkinter import ALL
+
 import cv2
+import numpy as np
+
 from Simulation import Simulation
-from tkinter import ALL, EventType
+from Vec2D import Vec2D
+from resizer import fit_tk, array_to_tk
 
 
 class GUI:
@@ -23,16 +27,27 @@ class GUI:
         self.preview_Label.image = self.preview_image
         self.preview_Label.place(x=25, y=50, width=200, height=200)
         # choose part
+        self.currentlychosen = 0
         self.preview_scrollbar = tk.Scrollbar(self.master, orient=tk.VERTICAL)
         self.part_options_listbox = tk.Listbox(self.master, selectmode=tk.SINGLE,
                                                yscrollcommand=self.preview_scrollbar.set)
         self.preview_scrollbar.config(command=self.part_options_listbox.yview)
         self.preview_scrollbar.place(x=25, y=255, height=50)
         self.part_options_listbox.place(x=40, y=255, width=180, height=50)
-        self.part_options_listbox.insert(0, "cross-section")
-        self.part_options_listbox.insert(1, "cross-section-x")
+        self.part_options_listbox.insert(0, "generate straight-road")
+        self.part_options_listbox.insert(1, "cross-section")
+        self.part_options_listbox.insert(2, "cross-section-x")
         self.load_button = tk.Button(master=self.master, text="Load model", command=self.load_part)
         self.load_button.place(x=25, y=310)
+        # straight road generation
+        self.straight_road_width_label = tk.Label(self.master, text="Width")
+        self.straight_road_length_label = tk.Label(self.master, text="Length")
+        self.straight_road_entry_width = tk.Entry(self.master)
+        self.straight_road_entry_length = tk.Entry(self.master)
+        self.straight_road_generate_button = tk.Button(self.master, text="Generate road",
+                                                       command=self.generate_straight_road)
+        self.straight_road_direction_label = tk.Label(self.master, text="Direction")
+        self.straight_road_entry_direction = tk.Entry(self.master)
         # canvas
         self.canvas_relwidth = 0.6
         self.canvas_relheight = 0.7
@@ -40,25 +55,70 @@ class GUI:
         self.model_preview.initialize_map()
         self.canvas_image = array_to_tk(self.model_preview.colormap)
         self.model_preview_desc = tk.Label(self.master, text="Preview of model")
-        self.model_preview_desc.place(x=250, y=25)
+        self.model_preview_desc.place(x=275, y=25)
         self.canvas_vertical_scroll = tk.Scrollbar(self.master, orient=tk.VERTICAL)
         self.canvas_horizontal_scroll = tk.Scrollbar(self.master, orient=tk.HORIZONTAL)
         self.canvas = tk.Canvas(master=self.master, width=self.model_preview.cellmap.shape[1],
                                 height=self.model_preview.cellmap.shape[0], borderwidth=5,
-                                background='white', yscrollcommand=self.canvas_vertical_scroll,
+                                background='gray', yscrollcommand=self.canvas_vertical_scroll,
                                 xscrollcommand=self.canvas_horizontal_scroll,
                                 scrollregion=(0, 0, self.model_preview.cellmap.shape[1],
                                               self.model_preview.cellmap.shape[0]))
         self.canvas_vertical_scroll.config(command=self.canvas.yview)
         self.canvas_horizontal_scroll.config(command=self.canvas.xview)
-        self.canvas_vertical_scroll.place(x=250, y=75, relheight=self.canvas_relheight)
-        self.canvas_horizontal_scroll.place(x=275, y=50, relwidth=self.canvas_relwidth)
-        self.canvas.place(x=275, y=75, relwidth=self.canvas_relwidth, relheight=self.canvas_relheight)
+        self.canvas_vertical_scroll.place(x=275, y=75, relheight=self.canvas_relheight)
+        self.canvas_horizontal_scroll.place(x=300, y=50, relwidth=self.canvas_relwidth)
+        self.canvas.place(x=300, y=75, relwidth=self.canvas_relwidth, relheight=self.canvas_relheight)
         self.canvas.create_image(0, 0, image=self.canvas_image, anchor=tk.NW)
         self.canvas.bind("<Button-1>", self.place_part)
         self.canvas.bind("<MouseWheel>", self.do_zoom)
         self.start_button = tk.Button(master=self.master, text="Start simulation", command=self.start_simulation)
-        self.start_button.place(x=400, y=20)
+        self.start_button.place(x=425, y=20)
+
+    def generate_straight_road(self):
+        try:
+            tmp = list(x.rstrip().lstrip() for x in self.straight_road_entry_direction.get().split(','))
+            assert len(tmp) < 3, msg.showerror("Error", "Too many axes")
+            x = int(tmp[0])
+            y = int(tmp[1])
+            assert -1 <= x <= 1 and -1 <= y <= 1, msg.showerror("Error", "Wrong direction, must be integer <-1:1>")
+            direction = Vec2D(x, y)
+            width = int(self.straight_road_entry_width.get())
+            length = int(self.straight_road_entry_length.get())
+            assert 0 < width <= 3 and 0 < length < self.model_preview.cellmap.shape[0] and length < \
+                self.model_preview.cellmap.shape[1], msg.showerror("Error", "Invalid length or width")
+            if width > 1:
+                assert width < length, msg.showerror("Error", "Invalid length or width")
+            self.part_preview = Simulation(length, length)
+
+            if direction.equal(Vec2D(1, 1)) or direction.equal(Vec2D(-1, -1)):
+                for i in range((width-1), length):
+                    for j in range(0, width):
+                        self.part_preview.cellmap[i-j, i+j-(width-1)].kind = "road"
+                        self.part_preview.cellmap[i-j, i+j-(width-1)].direction.append(direction)
+            elif direction.equal(Vec2D(1, -1)) or direction.equal(Vec2D(-1, 1)):
+                for i in range((width-1), length):
+                    for j in range(0, width):
+                        self.part_preview.cellmap[i-j, -i-j-1+(width-1)].kind = "road"
+                        self.part_preview.cellmap[i-j, -i-j-1+(width-1)].direction.append(direction)
+            elif direction.equal(Vec2D(0, 1)) or direction.equal(Vec2D(0, -1)):
+                for i in range(int(length/2) - floor(width/2), int(length/2) + ceil(width/2)):
+                    for cell in self.part_preview.cellmap[i, :]:
+                        cell.kind = "road"
+                        cell.direction.append(direction)
+            elif direction.equal(Vec2D(1, 0)) or direction.equal(Vec2D(-1, 0)):
+                for i in range(int(length / 2) - floor(width / 2), int(length / 2) + ceil(width / 2)):
+                    for cell in self.part_preview.cellmap[:, i]:
+                        cell.kind = "road"
+                        cell.direction.append(direction)
+
+            self.make_preview()
+
+        except ValueError:
+            msg.showerror("Error", "All entered values must be integer numbers")
+            return
+        except AssertionError:
+            return
 
     def make_preview(self):  # działa
         self.part_preview.initialize_map()
@@ -68,13 +128,34 @@ class GUI:
         self.preview_Label.image = f
 
     def load_part(self):  # działa
-        i = self.part_options_listbox.curselection()[0]
-        if i == 0:
+        self.currentlychosen = self.part_options_listbox.curselection()[0]
+        if self.currentlychosen != 0:
+            self.straight_road_width_label.place_forget()
+            self.straight_road_entry_width.place_forget()
+            self.straight_road_length_label.place_forget()
+            self.straight_road_entry_length.place_forget()
+            self.straight_road_direction_label.place_forget()
+            self.straight_road_entry_direction.place_forget()
+        if self.currentlychosen == 0:
+            self.part_preview = Simulation(1, 1)
+            self.make_preview()
+            self.straight_road_width_label.place(x=25, y=350)
+            self.straight_road_entry_width.place(x=100, y=350, width=50, height=25)
+            self.straight_road_entry_width.insert(0, "1")
+            self.straight_road_length_label.place(x=25, y=400)
+            self.straight_road_entry_length.place(x=100, y=400, width=50, height=25)
+            self.straight_road_entry_length.insert(0, "1")
+            self.straight_road_direction_label.place(x=25, y=450)
+            self.straight_road_entry_direction.place(x=100, y=450, width=50, height=25)
+            self.straight_road_entry_direction.insert(0, "1,1")
+            self.straight_road_generate_button.place(x=175, y=400)
+
+        elif self.currentlychosen == 1:
             array = np.load("cross-section.npy", allow_pickle=True)
             self.part_preview = Simulation(array.shape[0], array.shape[1])
             self.part_preview.cellmap = array
             self.make_preview()
-        elif i == 1:
+        elif self.currentlychosen == 2:
             array = np.load("cross-section-x.npy", allow_pickle=True)
             self.part_preview = Simulation(array.shape[0], array.shape[1])
             self.part_preview.cellmap = array
@@ -85,10 +166,10 @@ class GUI:
         row = int(canvas.canvasy(event.y))
         col = int(canvas.canvasx(event.x))
         if row + self.part_preview.cellmap.shape[1] > self.model_preview.cellmap.shape[1]:
-            msg.showerror("Part won't fit vertically")
+            msg.showerror("Error", "Part won't fit vertically")
             return
         if col + self.part_preview.cellmap.shape[0] > self.model_preview.cellmap.shape[0]:
-            msg.showerror("Part won't fit horizontally")
+            msg.showerror("Error", "Part won't fit horizontally")
             return
 
         # self.model_preview.cellmap[row:row+self.part_preview.cellmap.shape[1],
@@ -101,20 +182,20 @@ class GUI:
                     self.model_preview.cellmap[x, y] = partcell
                 elif modelcell.kind == "road" and partcell.kind == "road":  # bugs sometimes
                     # self.model_preview.cellmap[x, y].direction.append(self.part_preview.cellmap[x-row, y-col].direction[0])
-                    for dir in partcell.direction:
-                        if not any(dir.equal(direc) for direc in modelcell.direction):
-                            self.model_preview.cellmap[x, y].direction.append(dir)
+                    for dire in partcell.direction:
+                        if not any(dire.equal(direc) for direc in modelcell.direction):
+                            self.model_preview.cellmap[x, y].direction.append(dire)
 
-        # selected = []
-        # for x in range(row, row+self.part_preview.cellmap.shape[1]):
-        #     for y in range(col, col+self.part_preview.cellmap.shape[0]):
-        #         if self.model_preview.cellmap[x, y].kind == "road":
-        #             for dire in self.model_preview.cellmap[x, y].direction:
-        #                 nextcell = self.model_preview.cellmap[x+dire.x, y+dire.y]
-        #                 if nextcell.kind != "road":
-        #                     selected.append((x, y, dire))
-        # for x, y, dire in selected:
-        #     self.model_preview.cellmap[x, y].direction.remove(dire)
+        selected = []
+        for x in range(row, row+self.part_preview.cellmap.shape[1]):
+            for y in range(col, col+self.part_preview.cellmap.shape[0]):
+                if self.model_preview.cellmap[x, y].kind == "road":
+                    for dire in self.model_preview.cellmap[x, y].direction:
+                        nextcell = self.model_preview.cellmap[x+dire.x, y+dire.y]
+                        if nextcell.kind != "road":
+                            selected.append((x, y, dire))
+        for x, y, dire in selected:
+            self.model_preview.cellmap[x, y].direction.remove(dire)
 
         self.model_preview.initialize_map()
         self.canvas_image = array_to_tk(self.model_preview.colormap)
