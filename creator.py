898e407dc355cebe26b2172918ad86d4309cd212
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2020-05-05 13:54:15 +0200

    cross-T

--- creator.py
+++ creator.py
@@ -4,6 +4,9 @@ from Vec2D import Vec2D
 import numpy as np
 
 if __name__ == "__main__":
+    ###############################################################
+    # Cross section X (2 lanes)
+    ###############################################################
     mp = Simulation(100, 100, p=50)
 
     for cell in mp.cellmap[49, :]:
@@ -22,6 +25,9 @@ if __name__ == "__main__":
         cell.kind = "road"
         cell.direction.append(Vec2D(-1, 0))
 
+    np.save("cross-section-X.npy", mp.cellmap)
+
+
     # mp.add_car(pos=Vec2D(49, 5))
     # mp.add_car(pos=Vec2D(49, 1), vel=2)
     # mp.add_car(pos=Vec2D(5, 51))
@@ -36,7 +42,6 @@ if __name__ == "__main__":
     #     cell.kind = "road"
     #     cell.direction.append(Vec2D(1, 0))
     #
-    np.save("cross-section-x.npy", mp.cellmap)
     # mp.cellmap = np.load("cross-section.npy", allow_pickle=True)
     # mp.add_car(pos=Vec2D(25, 5))
     # mp.add_car(pos=Vec2D(25, 1), vel=2)
@@ -50,3 +55,96 @@ if __name__ == "__main__":
     #     k = cv2.waitKey(50)
     #     if k == 27:
     #         break
+
+
+    ###############################################################
+    # Cross section T up (2 lanes)
+    ###############################################################
+    T_up = Simulation(100, 100, p=50)
+
+    for cell in T_up.cellmap[0, :]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, -1))
+
+    for cell in T_up.cellmap[1:, 49]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(1, 0))
+
+    for cell in T_up.cellmap[2, :]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, 1))
+
+    for cell in T_up.cellmap[1:, 51]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(-1, 0))
+
+    np.save("cross-section-T-up.npy", T_up.cellmap)
+
+    ###############################################################
+    # Cross section T down (2 lanes)
+    ###############################################################
+    T_down = Simulation(100, 100, p=50)
+
+    for cell in T_down.cellmap[97, :]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, -1))
+
+    for cell in T_down.cellmap[:99, 49]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(1, 0))
+
+    for cell in T_down.cellmap[99, :]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, 1))
+
+    for cell in T_down.cellmap[:99, 51]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(-1, 0))
+
+    np.save("cross-section-T-down.npy", T_down.cellmap)
+
+    ###############################################################
+    # Cross section T left (2 lanes)
+    ###############################################################
+    T_left = Simulation(100, 100, p=50)
+
+    for cell in T_left.cellmap[49, 1:]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, -1))
+
+    for cell in T_left.cellmap[:, 0]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(1, 0))
+
+    for cell in T_left.cellmap[51, 1:]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, 1))
+
+    for cell in T_left.cellmap[:, 2]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(-1, 0))
+
+    np.save("cross-section-T-left.npy", T_left.cellmap)
+
+    ###############################################################
+    # Cross section T right (2 lanes)
+    ###############################################################
+    T_right = Simulation(100, 100, p=50)
+
+    for cell in T_right.cellmap[49, :99]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, -1))
+
+    for cell in T_right.cellmap[:, 97]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(1, 0))
+
+    for cell in T_right.cellmap[51, :99]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(0, 1))
+
+    for cell in T_right.cellmap[:, 99]:
+        cell.kind = "road"
+        cell.direction.append(Vec2D(-1, 0))
+
+    np.save("cross-section-T-right.npy", T_right.cellmap)
