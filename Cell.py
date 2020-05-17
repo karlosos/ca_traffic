3922603117a2bb9172d76130641390105ffde2d9
Artur <arturpietrzak522@o2.pl>
2020-05-17 19:18:41 +0200

    Zmiana sposobu generacji dróg + usprawnienia generatora
    Generowanie poszczególnych dróg zostało przeniesione z plików .npy do pliku creator.py. Plik creator.py służy teraz do definiowania metod służących do generacji poszczególnych klocków. Zabieg ma zapobiec wczytywaniu starych definicji klas przy użyciu numpy.load.
    
    Usprawniony został generator samochodów, teraz za pozycje startowe będą traktowane wszystkie te punkty, do których można się dostać jedynie poruszając się w kierunku przeciwnym do kierunku drogi (samochody nie mają jak dojechać do tego punktu, ale mogą z niego wyjechać).
    
    Dodatkowo zaimplementowany jest początek metody generującej mapę cieplną (jeszcze nic nie robi, ale już wkrótce będzie).

--- Cell.py
+++ Cell.py
@@ -2,6 +2,8 @@ class Cell:
     def __init__(self, car=None, kind=None, direction=None):
         if direction is None:
             direction = []
+
         self.car = car
         self.kind = kind
         self.direction = direction
+        self.visited = 0
