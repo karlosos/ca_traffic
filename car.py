85821c4798a70e22145a2795ee88929daf67e6e9
Artur <arturpietrzak522@o2.pl>
2020-05-20 14:37:43 +0200

    Changes to car.py, gui.py and Simulation.py
    Dodano zbieranie pomiarów prędkości samochodów od momentu ich utworzenia.

--- car.py
+++ car.py
@@ -2,8 +2,9 @@ from Vec2D import Vec2D
 
 
 class Car:
-    def __init__(self, velocity=1, position=Vec2D()):
+    def __init__(self, velocity=1, position=Vec2D(), idx=-1):
         self.position = position
         self.velocity = velocity
         self.oldDirection = Vec2D()
         self.defaultvelocity = velocity
+        self.idx = idx
